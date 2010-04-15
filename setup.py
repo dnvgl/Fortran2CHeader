628d83a04e130b452f3bd44e2cb1424021071d9d
Berthold Höllmann <berthold.hoellmann@dnvgl.com>
2010-04-15 13:55:58 +0000

    added setup.py
    git-svn-id: https://svnsrv/svn/Fortran2CHeader/trunk@4 a2318b8c-2c9a-43ed-b7cd-801b4c6247bb

---
+++ setup.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+u"""
+Install the fortran2cheader tool.
+
+:author: `Berthold Hoellmann <hoel@GL-group.com>`__
+:newfield project: Project
+:project: Fortran2CHeader
+:copyright: Copyright (C) 2010 by Germanischer Lloyd AG"""
+
+#  ID: $Id$
+__date__      = u"$Date$"[5:-1]
+__version__   = "$Revision$"[10:-1]
+__docformat__ = "restructuredtext en"
+
+from distutils.core import setup
+
+setup(name='Fortran2CHeader',
+      version='0.1',
+      scripts=['fortran2cheader.py'],
+      )
+
+# Local Variables:
+# mode:python
+# mode:flyspell
+# compile-command:"python setup.py build"
+# End:
